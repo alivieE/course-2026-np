e746f39a33927e719767cbd480ad00d7a02d0cb9
alivieE <153372860+alivieE@users.noreply.github.com>
2026-02-08 00:33:47 +0200

    test push

--- src/main/java/ua/com/kisit/course2026np/Course2026NpApplication.java
+++ src/main/java/ua/com/kisit/course2026np/Course2026NpApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Course2026NpApplication {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         SpringApplication.run(Course2026NpApplication.class, args);
     }
 

0fa58c4f1a8a95516d9e16019cfc1cd263cb6329
alivieE <153372860+alivieE@users.noreply.github.com>
2026-02-08 00:26:50 +0200

    fp

---
+++ src/main/java/ua/com/kisit/course2026np/Course2026NpApplication.java
@@ -0,0 +1,13 @@
+package ua.com.kisit.course2026np;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2026NpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Course2026NpApplication.class, args);
+    }
+
+}

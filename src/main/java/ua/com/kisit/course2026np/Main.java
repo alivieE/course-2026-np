f675aa2c6f211bb4b89595515852b76e80c352fa
alivieE <153372860+alivieE@users.noreply.github.com>
2026-04-07 20:22:26 +0300

    lr 6

--- src/main/java/ua/com/kisit/course2026np/Main.java
+++ src/main/java/ua/com/kisit/course2026np/Main.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        // Запуск Spring Boot
         SpringApplication.run(Main.class, args);
+
+        // Вивід красивого повідомлення з посиланням після успішного запуску
+        System.out.println("\n");
+        System.out.println("🚀 Проект успішно запущено!");
+        System.out.println("👉 Натисніть на посилання, щоб відкрити сайт: http://localhost:8080/");
+        System.out.println("\n");
     }
 }

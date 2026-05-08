1ec5436bc8ff21adc4c4c8df6bede3403264c9e8
alivieE <153372860+alivieE@users.noreply.github.com>
2026-05-08 18:50:19 +0300

    blocker fix

--- src/main/java/ua/com/kisit/course2026np/Main.java
+++ src/main/java/ua/com/kisit/course2026np/Main.java
@@ -3,6 +3,9 @@ package ua.com.kisit.course2026np;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
